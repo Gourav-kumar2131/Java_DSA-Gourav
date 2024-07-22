d23e89c81d03b032d978702021a271dbdc3e6326
Gourav Kumar <gouravkrd2131@gmail.com>
2024-07-23 03:40:06 +0530

    Hash Map

---
+++ DSA_Java_Kunal/src/Hash/Hash_Map.java
@@ -0,0 +1,40 @@
+package Hash;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Hash_Map {
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+
+        // .put(K, v), .get( K ), .containsKey(K), remove(K), size(), .keySet()-- iterate
+        map.put("Gourav", 133);
+        map.put("Kunal", 144);
+        map.put("Shradha", 155);
+        map.put("Gourav", 200); // if key already exist then, update the value of key associate with it
+//
+//        System.out.println( map.get("Gourav"));
+//
+//        System.out.println(map.containsKey("Kunal"));
+//
+//        System.out.println(map.remove("Gourav")); // if not exist return null
+//
+//        System.out.println(map.size());
+
+//        1st way to iterate the Hashmap.
+
+        for( Map.Entry<String , Integer> e : map.entrySet()){
+            System.out.println(e.getKey());
+        }
+
+//        2nd way to iterate the Hashmap.
+        Set<String> mapkey = map.keySet();
+        for(String key : mapkey){
+            System.out.println(key+ " "+ map.get(key));
+        }
+
+
+
+
+    }
+}
