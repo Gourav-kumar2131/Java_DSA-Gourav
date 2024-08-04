0b046f469ff00292db387f61f9f7d74843a24a10
Gourav Kumar <gouravkrd2131@gmail.com>
2024-08-04 21:42:21 +0530

    arrayList modified

--- DSA_Java_Kunal/src/Array_Arraylist/ArrayList_Def.java
+++ DSA_Java_Kunal/src/Array_Arraylist/ArrayList_Def.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class ArrayList_Def {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-         // full covered in OOps array_String builder
+         // full covered in OOps array_Stringbuilder
 
         ArrayList<Integer> list = new ArrayList<Integer>(10); // 10 col is constructor we can create more objects than initial capacity
 
@@ -15,9 +15,10 @@ public class ArrayList_Def {
         for(int i=0; i<3; i++){
             list2.add( new ArrayList<>());
         }
-        for(int i=0; i<3; i++){
-            list2.get(i).add(in.nextInt());
-        }
+//        for(int i=0; i<3; i++){
+//            list2.get(i).add(in.nextInt());
+//        }
+        System.out.println('a');
 
 
     }
--- DSA_Java_Kunal/src/Array_Arraylist/Array_2D.java
+++ DSA_Java_Kunal/src/Array_Arraylist/Array_2D.java
@@ -6,12 +6,14 @@ public class Array_2D {
     public static void main(String[] args){
 //        Declaration of 2D array
         int[][] array = new int[3][]; // column size is not mandatory but row size is mandatory
+        int[][] nums = new int[7][];
 
         int[][] arr2 = {
                 {1, 2, 3},
                 {3, 4, 5, 9},
                 {6, 7 ,8},
         };
+
         // printing the element of the 2D array
         for(int row=0; row < arr2.length ; row++){
             for (int col=0; col<arr2[row].length; col++){
@@ -20,6 +22,7 @@ public class Array_2D {
             }
             System.out.println(" ");
         }
+
         //  2nd Method to print
         for(int[] arr: arr2){
             System.out.println(Arrays.toString(arr));
